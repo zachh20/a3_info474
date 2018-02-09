0b0439ceff8935495be9de4a6eb24900a08d4823
Jon Alkan <alkanj@uw.edu>
2018-02-08 16:03:13 -0800

    small stuff

---
+++ a3_info474/d3script.js
@@ -0,0 +1,25 @@
+var margin = {top: 20, right: 20, bottom: 20, left: 20},
+    width = +400- margin.left - margin.right,
+height = +400 - margin.top - margin.bottom
+
+var svg = d3.select("svg")
+    .append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+var xScale = d3.scaleLinear()
+.domain([0, d3.max(dataset, function(d) { return d[0]})])
+.range([0,width]);
+
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset, function(d) { return d[1]})])
+    .range([height,0]);
+
+svg.selectAll("circle") 
+    .data(dataset)  
+    .enter().append("circle")
+    .attr("cx", function(d) { return xScale(d[0]); })  
+    .attr("cy", function(d) { return yScale(d[1]); }) 
+    .attr("r", 10);
+
+
