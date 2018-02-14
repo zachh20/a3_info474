9182b00422f6c412f7f00b4b6a62606f92d87fef
zachh20 <zachh20@msn.com>
2018-02-13 16:37:12 -0800

    we made a title

--- a3_info474/d3script.js
+++ a3_info474/d3script.js
@@ -10,25 +10,55 @@ var svg = d3.select("svg")
 
 d3.csv('data.csv', function(data){
     var body = d3.select('body')
+    drawVis(data)
+
+
+    
     var selectedData = []
     for (var i = 0; i < data.length; i++){
         selectedData.push([data[i].FullTimeUnemploymentRate, data[i].Median])
     }
-
-    var mouseData = [] 
+        var mouseData = [] 
     for (var i = 0; i < data.length; i++){
-        mouseData.push(["Unemployment Rate: " + data[i].FullTimeUnemploymentRate,
-                        "Median Salary: " + data[i].Median, 
-                        "Major Category: " + data[i].Major_category, 
+        mouseData.push(["Unemployment Rate: " + data[i].FullTimeUnemploymentRate + '<br />' +
+                        "Median Salary: " + data[i].Median + '<br />' +
+                        "Major Category: " + data[i].Major_category + '<br />' +
                         "Major: " + data[i].Major])
     }
 
+
+     d3.select("#selectform")
+        .on('change', function(){
+        var majorCat = document.getElementById('myselectform')
+        console.log("changy boi is working");
+        filterType(majorCat);
+        console.log("changy boi is working");
+    })
+
+     function filterType(mtype){ 
+
+        var res = patt.test(mtype);
+        if(res && mtype == 'none'){
+            drawVis(data);
+        }else{
+            var ndata = data.filter(function(d, i) {
+                return data[i].Major_category == mtype;
+            });
+            drawVis(ndata)
+        }
+    }
+
+   
+
+   
+
+    function drawVis(data) {
 	var xScale = d3.scaleLinear()
-		.domain([0, d3.max(selectedData, function(d) { return d[0]})])
+		.domain([0, d3.max(data, function(d, i) { return data[i].FullTimeUnemploymentRate})])
 		.range([0,width]);
 
 	var yScale = d3.scaleLinear()
-		.domain([0, d3.max(selectedData, function(d) { return d[1]})])
+		.domain([30000, d3.max(data, function(d, i) { return data[i].Median})])
 		.range([height,0]);
 
     var div = d3.select("body").append("div")	
@@ -36,19 +66,19 @@ d3.csv('data.csv', function(data){
     .style("opacity", 0);
 
 	svg.selectAll("circle") 
-		.data(selectedData)  
+		.data(data)  
 		.enter().append("circle")
-		.attr("cx", function(d) { return xScale(d[0]); })  
-		.attr("cy", function(d) { return yScale(d[1]); }) 
-		.attr("r", 10)
-        .style("fill", function(d, i) {console.log(data[i].Major_category); return col(data[i].Major_category); })
-        .on("mouseover", function(d) {		
+		.attr("cx", function(d, i) { return xScale(data[i].FullTimeUnemploymentRate); })  
+		.attr("cy", function(d, i) { return yScale(data[i].Median); }) 
+		.attr("r", 7)
+        .style("fill", function(d, i) { return col(data[i].Major_category); })
+        .on("mouseover", function(d, i) {		
             div.transition()		
                 .duration(200)		
                 .style("opacity", .9);		
-            div	.html(mouseData[selectedData.indexOf(d)])	
+            div	.html(mouseData[i])	
                 .style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY - 28) + "px");	
+                .style("top", (d3.event.pageY - 28) + "px");
             })					
         .on("mouseout", function(d) {		
             div.transition()		
@@ -63,10 +93,8 @@ d3.csv('data.csv', function(data){
       svg.append("g")
        .call(d3.axisLeft(yScale));
 
-	circles = svg.selectAll("circle") 
-		.data(selectedData) 
-		.attr("fill","black") 
-		.attr("r",3);
+	
+    }
 
 })
     
